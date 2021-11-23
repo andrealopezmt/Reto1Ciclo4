1562e688d8949045d1cd52835db15da25585e0aa
andrealopezmt <andrea.lopez.mt@correo.usa.edu.co>
2021-11-22 22:34:28 -0500

    first commit

---
+++ src/main/java/Reto1_Ciclo4/Reto1Ciclo4v2/Reto1Ciclo4v2Application.java
@@ -0,0 +1,13 @@
+package Reto1_Ciclo4.Reto1Ciclo4v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto1Ciclo4v2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto1Ciclo4v2Application.class, args);
+	}
+
+}
---
+++ src/test/java/Reto1_Ciclo4/Reto1Ciclo4v2/Reto1Ciclo4v2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Reto1_Ciclo4.Reto1Ciclo4v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto1Ciclo4v2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
